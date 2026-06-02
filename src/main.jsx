132b2dd0e2b17e6e0b44d2e13e9a48fb1fc2f5f4
Gustavo <gustavohgo@hotmail.com>
2026-06-02 02:41:02 -0300

    Fix React Router basename for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/produz-facil">
       <App />
     </BrowserRouter>
   </React.StrictMode>
